d2d53d1c8a59058a58e3120b442a3012de0ee10f
Brice WeKeys <formation@wekeys.fr>
2026-03-05 14:53:42 +0100

    Ajout du cookie

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//Creation d'un cookie
+function createCookie() { 
+  var now = new Date();
+  var time = now.getTime();
+  var expireTime = time + 1000*36000;
+  now.setTime(expireTime);
+  number =  Math.floor(Math.random() * 50000);
+    (!document.cookie) ? document.cookie = 'cookie='+number+';expires='+now.toUTCString()+';path=/' : '';
+  }
+createCookie();
 document.getElementById('date').valueAsDate = new Date();
+
+let myCookie = document.cookie;
