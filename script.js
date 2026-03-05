5a78824d1da774b63c0c0849422e4a18c0cd939d
Brice WeKeys <formation@wekeys.fr>
2026-03-05 18:57:28 +0100

    Ajout rapide des resultats

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function createCookie() {
 createCookie();
 document.getElementById('date').valueAsDate = new Date();
 
+//Création div des résultat
+function createDivResult(object) {
+    //faire le calcule de l'heure du voyage
+    let heure = ''; //Moment
+    document.getElementById('card-right').innerHTML += `
+    <div class="trip">
+      <div class="trajetName">${object.departure} > ${object.arrival}</div>
+      <div class="heure">${heure}</div>
+      <div class="prix">${object.price}</div>
+      <div class="book-btn">Book</div>
+    </div>`
+} 
+
 let myCookie = document.cookie;
 document.getElementById('btn-search').addEventListener('click', function() {
     let departure = document.querySelector('#depart').value;
@@ -18,5 +31,10 @@ document.getElementById('btn-search').addEventListener('click', function() {
     console.log(departure + ' ' + arrival + ' ' + date);
     fetch('http://localhost:3000/trips/request/' + departure +'/'+arrival+'/'+date)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      if (data.result) {
+        document.querySelector('.trip').remove();
+        data.trips.forEach(element => createDivResult(element))
+      }
+    })
 })
