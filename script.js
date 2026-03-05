e04e24284a1aab8a3bcd3a2003cc7675148c8194
Brice WeKeys <formation@wekeys.fr>
2026-03-05 20:11:06 +0100

    ajout de l'heure

--- script.js
+++ script.js
@@ -13,18 +13,29 @@ document.getElementById('date').valueAsDate = new Date();
 //Création div des résultat
 function createDivResult(object) {
     //faire le calcule de l'heure du voyage
-    let heure = ''; //Moment
+    let heure = moment(object.date['$date']).format('HH:mm'); //Moment
     document.getElementById('card-right').innerHTML += `
     <div class="voyage">
       <div class="trajetName">${object.departure} > ${object.arrival}</div>
       <div class="heure">${heure}</div>
       <div class="prix">${object.price}</div>
+      <div style="content-visibility: hidden">${object.date['$date']}</div>
       <div class="book-btn"><button type="button">Book</div>
     </div>`
 } 
 
+function addToCart(object, cookie){
+  let {trajet, date, prix} = object;
+  console.log(trajet)
+  fetch('http://localhost:3000/trips/addtocart', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+		body: JSON.stringify({ cookie: cookie, trajet: trajet, date: date, prix: prix }),
+	}).then(response => response.json()).then(data => console.log(data))
+}
+
 let myCookie = document.cookie;
-document.getElementById('btn-search').addEventListener('click', function() {
+document.getElementById('btn-search').addEventListener('click',function() {
     let departure = document.querySelector('#depart').value;
     let arrival = document.querySelector('#arrivee').value;
     let date = document.querySelector('#date').value;
@@ -33,12 +44,26 @@ document.getElementById('btn-search').addEventListener('click', function() {
     .then(response => response.json())
     .then(data => {
       if (data.result) {
+        console.log(data)
         document.querySelector('.trip').remove();
         data.trips.forEach(element => createDivResult(element))
       }
-    })
+    });
+    let x = document.getElementById('card-right')
+    document.getElementById('card-right').addEventListener('click', async (e) => {
+      if(e.target.parentElement.classList.contains('book-btn')) {
+        console.log(e.target.parentElement.parentElement);
+        let cartObject = {};
+        cartObject = {
+          trajet: e.target.parentElement.parentElement.children[0].innerText,
+          date: e.target.parentElement.parentElement.children[3].firstChild.data,
+          prix: e.target.parentElement.parentElement.children[2].innerText
+        }
+        let cookie = document.cookie;
+        console.log(cartObject + cookie)
+        await addToCart(cartObject, cookie)
+      }
 });
 
-document.getElementById('card-right').addEventListener('click', function() {
-  console.log(this);
-})
+
+});
